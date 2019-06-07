a220d2c2660ff09d67304241168373a295acdabf
Nate Magera <nmagera07@gmail.com>
2019-06-07 11:42:40 -0400

    finished the cards component files

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
-import PropTypes from 'prop-types'
+import propTypes from 'prop-types'
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -8,12 +8,11 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-            props.tabs.map(tab => {
-              <Tab 
-                selectTabHandler={props.selectTabHandler} 
-                selectedTab={props.selectedTab} 
-                git tab={tab} />
-            })
+            {props.tabs.map( tab => {
+              return(
+                <Tab selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} tab={tab} />
+              )
+            })}
       </div>
     </div>
   );
